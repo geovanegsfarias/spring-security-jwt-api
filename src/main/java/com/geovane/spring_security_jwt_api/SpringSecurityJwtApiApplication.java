2ee7b70dc32754d4c5fa913364d91779988730cb
Geovane <geovanegsf02@gmail.com>
2026-03-20 19:30:46 -0300

    Initial commit

---
+++ src/main/java/com/geovane/spring_security_jwt_api/SpringSecurityJwtApiApplication.java
@@ -0,0 +1,13 @@
+package com.geovane.spring_security_jwt_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityJwtApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityJwtApiApplication.class, args);
+	}
+
+}
